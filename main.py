21dfdf255b92d0b482e2c71a81f13eddd31d9b3a
Chris Johnedis <christopher.johnedis@gmail.com>
2024-11-24 13:45:32 -0800

    update main.py
    add function for counting instances of a character

--- main.py
+++ main.py
@@ -1,13 +1,27 @@
-def count(text):
+def count_word(text):
     text_array = text.split()
     return len(text_array)
+
+
+def count_char(text):
+    char_dict = {}
+    for char in text:
+        low_char = char.lower()
+        if low_char.isalpha():
+            if low_char in char_dict:
+                char_dict[low_char] = (char_dict[low_char] + 1)
+            else:
+                char_dict[low_char] = 1
+    return char_dict
 
 
 def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
-        number = count(file_contents)
-        print(number)
+        word_num = count_word(file_contents)
+        char_num = count_char(file_contents)
+        print(word_num)
+        print(char_num)
 
 
 main()
