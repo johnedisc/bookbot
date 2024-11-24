7e6e2a533a425c4ab64c96a60635a8e162d93028
Chris Johnedis <christopher.johnedis@gmail.com>
2024-11-23 17:11:56 -0800

    main.py
    add function to count words

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
+def count(text):
+    text_array = text.split()
+    return len(text_array)
+
+
 def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
-        print(file_contents)
+        number = count(file_contents)
+        print(number)
 
 
 main()
